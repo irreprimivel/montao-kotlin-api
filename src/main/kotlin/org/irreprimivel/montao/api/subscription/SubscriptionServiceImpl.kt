7ab4b655d5fba9065866b5b596a6f4d572dd219f
truesik <truesik@gmail.com>
2017-04-10 23:08:17 +0300

    функциональцина

--- src/main/kotlin/org/irreprimivel/montao/api/subscription/SubscriptionServiceImpl.kt
+++ src/main/kotlin/org/irreprimivel/montao/api/subscription/SubscriptionServiceImpl.kt
@@ -2,21 +2,17 @@ package org.irreprimivel.montao.api.subscription
 
 import org.irreprimivel.montao.api.community.entity.Community
 import org.irreprimivel.montao.api.user.User
+import org.springframework.stereotype.Service
 
+@Service
 class SubscriptionServiceImpl(val subscriptionDAO: SubscriptionDAO) : SubscriptionService {
-    override fun add(subscription: Subscription) {
-        subscriptionDAO.add(subscription)
-    }
+    override fun add(subscription: Subscription) = subscriptionDAO.add(subscription)
 
-    override fun delete(subscription: Subscription) {
-        subscriptionDAO.delete(subscription)
-    }
+    override fun delete(subscription: Subscription) = subscriptionDAO.delete(subscription)
 
-    override fun getByUser(user: User, page: Int, limit: Int): List<Community> {
-        return subscriptionDAO.getByUser(user, page, limit)
-    }
+    override fun getByUser(user: User, page: Int, limit: Int): List<Community> = subscriptionDAO
+            .getByUser(user, page, limit)
 
-    override fun getByCommunity(community: Community, page: Int, limit: Int): List<User> {
-        return subscriptionDAO.getByCommunity(community, page, limit)
-    }
+    override fun getByCommunity(community: Community, page: Int, limit: Int): List<User> = subscriptionDAO
+            .getByCommunity(community, page, limit)
 }
