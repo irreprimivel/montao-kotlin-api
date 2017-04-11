9df28a8b33d7c9ef5fd133a4e58a23eed3032ae7
truesik <truesik@gmail.com>
2017-04-11 21:46:57 +0300

    Добавил поля к энтити канала

--- src/main/kotlin/org/irreprimivel/montao/api/channel/entity/Channel.kt
+++ src/main/kotlin/org/irreprimivel/montao/api/channel/entity/Channel.kt
@@ -1,9 +1,25 @@
 package org.irreprimivel.montao.api.channel.entity
 
-import javax.persistence.Entity
-import javax.persistence.Table
+import com.fasterxml.jackson.annotation.JsonBackReference
+import org.hibernate.validator.constraints.NotEmpty
+import org.irreprimivel.montao.api.community.entity.Community
+import javax.persistence.*
 
 @Entity
 @Table(name = "channels")
 data class Channel(
-        val id: Long)
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        val id: Long,
+
+        @Column(name = "title", nullable = false)
+        @NotEmpty
+        val title: String,
+
+        @Column(name = "description")
+        val description: String,
+
+        @JsonBackReference
+        @ManyToOne
+        @JoinColumn(name = "community_id")
+        val community: Community)
