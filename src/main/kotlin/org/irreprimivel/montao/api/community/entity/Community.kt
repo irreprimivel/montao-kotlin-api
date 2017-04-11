4285931e04019a3b44b40ede1277e05809fd47bb
truesik <truesik@gmail.com>
2017-04-11 22:24:52 +0300

    Добавил поля в энтити сообщества

--- src/main/kotlin/org/irreprimivel/montao/api/community/entity/Community.kt
+++ src/main/kotlin/org/irreprimivel/montao/api/community/entity/Community.kt
@@ -1,3 +1,41 @@
 package org.irreprimivel.montao.api.community.entity
 
-data class Community(val id: Long)
+import com.fasterxml.jackson.annotation.JsonManagedReference
+import org.hibernate.validator.constraints.NotEmpty
+import org.irreprimivel.montao.api.channel.entity.Channel
+import org.irreprimivel.montao.api.user.User
+import java.math.BigInteger
+import java.time.LocalDate
+import javax.persistence.*
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
+@Entity
+@Table(name = "communities")
+data class Community(
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        val id: BigInteger? = null,
+
+        @Column(name = "title", nullable = false, unique = true)
+        @NotEmpty
+        @Size(min = 4)
+        val title: String? = null,
+
+        @Column(name = "description")
+        val description: String? = null,
+
+        @Column(name = "creation_date", nullable = false)
+        val creationDate: LocalDate? = null,
+
+        @ManyToOne
+        @JoinColumn(name = "founder_id")
+        val founder: User? = null,
+
+        @Column(name = "is_visible", nullable = false)
+        @NotNull
+        val isVisible: Boolean? = null,
+
+        @JsonManagedReference
+        @OneToMany(mappedBy = "community", fetch = FetchType.EAGER)
+        val channels: List<Channel>? = null)
