be78613f364100bca3083dff384b5a1c33205bc2
truesik <truesik@gmail.com>
2017-04-11 21:53:51 +0300

    Запиливаем контроллер для сообществ

---
+++ src/main/kotlin/org/irreprimivel/montao/api/community/CommunityRestController.kt
@@ -0,0 +1,9 @@
+package org.irreprimivel.montao.api.community
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(value = "/communities")
+class CommunityRestController {
+}
