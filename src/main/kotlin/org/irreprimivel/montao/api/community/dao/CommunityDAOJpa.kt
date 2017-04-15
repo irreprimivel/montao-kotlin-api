030a54f2e455ba495049555f6fd1db89f9a91ea7
truesik <truesik@gmail.com>
2017-04-15 21:28:51 +0300

    Добавил реализацию для методов дао сообществ

--- src/main/kotlin/org/irreprimivel/montao/api/community/dao/CommunityDAOJpa.kt
+++ src/main/kotlin/org/irreprimivel/montao/api/community/dao/CommunityDAOJpa.kt
@@ -1,25 +1,35 @@
 package org.irreprimivel.montao.api.community.dao
 
 import org.irreprimivel.montao.api.community.entity.Community
+import org.irreprimivel.montao.api.community.exception.CommunityNotFoundException
 import org.springframework.stereotype.Repository
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
 
 @Repository
-class CommunityDAOJpa : CommunityDAO {
-    override fun add(community: Community) {
-    }
+class CommunityDAOJpa(@PersistenceContext val entityManager: EntityManager) : CommunityDAO {
+    override fun add(community: Community) = entityManager.persist(community)
 
-    override fun delete(community: Community) {
-    }
+    override fun delete(community: Community) = entityManager.remove(community)
 
-    override fun update(community: Community): Community {
-    }
+    override fun update(community: Community): Community = entityManager.merge(community)
 
-    override fun getAll(page: Int, limit: Int): List<Community> {
-    }
+    override fun getAll(page: Int, limit: Int): List<Community> = entityManager
+            .createQuery("select Community from Community", Community::class.java)
+            .setMaxResults(limit)
+            .setFirstResult((page - 1) * limit)
+            .resultList
 
-    override fun getByTitle(title: String): Community {
-    }
+    override fun getByTitle(title: String): Community = entityManager
+            .createQuery("select Community from Community where title = :title", Community::class.java)
+            .setParameter("title", title)
+            .resultList.stream()
+            .findFirst()
+            .orElseThrow {
+                CommunityNotFoundException("Community with $title title not found")
+            }
 
-    override fun totalCount(): Long {
-    }
+    override fun totalCount(): Long = entityManager.createQuery("select id from Community", Community::class.java)
+            .maxResults
+            .toLong()
 }
