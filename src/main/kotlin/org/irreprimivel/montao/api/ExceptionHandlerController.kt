e59898a7b8e9094df48583123d1c2d057b9f7818
truesik <truesik@gmail.com>
2017-04-17 23:14:25 +0300

    Контроллер который занимается перехватом ошибок и отправкой их клиенту по ресту.

---
+++ src/main/kotlin/org/irreprimivel/montao/api/ExceptionHandlerController.kt
@@ -0,0 +1,25 @@
+package org.irreprimivel.montao.api
+
+import org.irreprimivel.montao.api.channel.exception.ChannelNotFoundException
+import org.irreprimivel.montao.api.community.exception.CommunityNotFoundException
+import org.irreprimivel.montao.api.model.Error
+import org.irreprimivel.montao.api.user.exception.UserNotFoundException
+import org.springframework.http.HttpStatus.*
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class ExceptionHandlerController {
+    @ExceptionHandler(CommunityNotFoundException::class)
+    @ResponseStatus(NOT_FOUND)
+    fun communityNotFound(e: CommunityNotFoundException): Error = Error(NOT_FOUND.value(), e.message!!)
+
+    @ExceptionHandler(ChannelNotFoundException::class)
+    @ResponseStatus(NOT_FOUND)
+    fun communityNotFound(e: ChannelNotFoundException): Error = Error(NOT_FOUND.value(), e.message!!)
+
+    @ExceptionHandler(UserNotFoundException::class)
+    @ResponseStatus(NOT_FOUND)
+    fun communityNotFound(e: UserNotFoundException): Error = Error(NOT_FOUND.value(), e.message!!)
+}
