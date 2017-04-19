4d8d7028864b1fc1ebbad5277656b605a1ba145d
truesik <truesik@gmail.com>
2017-04-19 22:18:18 +0300

    Дописал два метода и подчистил код

--- src/main/kotlin/org/irreprimivel/montao/api/channel/controller/ChannelRestController.kt
+++ src/main/kotlin/org/irreprimivel/montao/api/channel/controller/ChannelRestController.kt
@@ -2,8 +2,9 @@ package org.irreprimivel.montao.api.channel.controller
 
 import org.irreprimivel.montao.api.channel.entity.Channel
 import org.irreprimivel.montao.api.channel.service.ChannelService
+import org.irreprimivel.montao.api.message.entity.Message
 import org.springframework.http.HttpHeaders
-import org.springframework.http.MediaType
+import org.springframework.http.MediaType.*
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.util.UriComponentsBuilder
@@ -21,25 +22,23 @@ import org.springframework.web.util.UriComponentsBuilder
 @RestController
 @RequestMapping(value = "/channels")
 class ChannelRestController(val channelService: ChannelService) {
-    @PostMapping(produces = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE))
+    @PostMapping(consumes = arrayOf(APPLICATION_JSON_UTF8_VALUE), produces = arrayOf(APPLICATION_JSON_UTF8_VALUE))
     fun add(@RequestBody channel: Channel, uriComponentsBuilder: UriComponentsBuilder): ResponseEntity<Channel> {
         channelService.add(channel)
         val location = uriComponentsBuilder.path("/channels/{title}").buildAndExpand(channel.title).toUri()
         return ResponseEntity.created(location).build()
     }
 
-    @PutMapping(consumes = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE),
-                produces = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE))
+    @PutMapping(consumes = arrayOf(APPLICATION_JSON_UTF8_VALUE), produces = arrayOf(APPLICATION_JSON_UTF8_VALUE))
     fun update(@RequestBody channel: Channel): ResponseEntity<Channel> = ResponseEntity.ok(channelService.update(channel))
 
-    @DeleteMapping(consumes = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE),
-                   produces = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE))
+    @DeleteMapping(consumes = arrayOf(APPLICATION_JSON_UTF8_VALUE), produces = arrayOf(APPLICATION_JSON_UTF8_VALUE))
     fun delete(@RequestBody channel: Channel): ResponseEntity<Channel> {
         channelService.delete(channel)
         return ResponseEntity.ok(channel)
     }
 
-    @GetMapping(produces = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE))
+    @GetMapping(produces = arrayOf(APPLICATION_JSON_UTF8_VALUE))
     fun getAll(page: Int = 1, limit: Int = 30): ResponseEntity<List<Channel>> {
         val headers = HttpHeaders()
         with(headers) {
@@ -50,7 +49,11 @@ class ChannelRestController(val channelService: ChannelService) {
         return ResponseEntity.ok(channelService.getAll(page, limit))
     }
 
-    @GetMapping(value = "/{title}", produces = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE))
-    fun getByTitle(@PathVariable title: String): ResponseEntity<Channel> = ResponseEntity.ok(channelService.getByTitle(
-            title))
+    @GetMapping(value = "/{title}", produces = arrayOf(APPLICATION_JSON_UTF8_VALUE))
+    fun getByTitle(@PathVariable title: String): ResponseEntity<Channel> = ResponseEntity
+            .ok(channelService.getByTitle(title))
+
+    @GetMapping(value = "/{title}/messages", produces = arrayOf(APPLICATION_JSON_UTF8_VALUE))
+    fun getMessagesByChannel(@PathVariable title: String): ResponseEntity<List<Message>> = ResponseEntity
+            .ok(channelService.getByTitle(title).messages)
 }
