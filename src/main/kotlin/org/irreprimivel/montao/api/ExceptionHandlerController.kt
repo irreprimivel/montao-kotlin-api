6c8be5d053b6edc42a369b8d2d9fe580bdc61bf0
truesik <truesik@gmail.com>
2017-04-23 22:43:54 +0300

    Оставил только одну ошибку на всех

--- src/main/kotlin/org/irreprimivel/montao/api/ExceptionHandlerController.kt
+++ src/main/kotlin/org/irreprimivel/montao/api/ExceptionHandlerController.kt
@@ -1,30 +1,15 @@
 package org.irreprimivel.montao.api
 
-import org.irreprimivel.montao.api.channel.exception.ChannelNotFoundException
-import org.irreprimivel.montao.api.community.exception.CommunityNotFoundException
-import org.irreprimivel.montao.api.message.exception.MessageNotFoundException
 import org.irreprimivel.montao.api.model.Error
-import org.irreprimivel.montao.api.user.exception.UserNotFoundException
 import org.springframework.http.HttpStatus.*
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.util.NoSuchElementException
 
 @RestControllerAdvice
 class ExceptionHandlerController {
-    @ExceptionHandler(CommunityNotFoundException::class)
+    @ExceptionHandler(NoSuchElementException::class)
     @ResponseStatus(NOT_FOUND)
-    fun communityNotFound(e: CommunityNotFoundException): Error = Error(NOT_FOUND.value(), e.message!!)
-
-    @ExceptionHandler(ChannelNotFoundException::class)
-    @ResponseStatus(NOT_FOUND)
-    fun communityNotFound(e: ChannelNotFoundException): Error = Error(NOT_FOUND.value(), e.message!!)
-
-    @ExceptionHandler(UserNotFoundException::class)
-    @ResponseStatus(NOT_FOUND)
-    fun communityNotFound(e: UserNotFoundException): Error = Error(NOT_FOUND.value(), e.message!!)
-
-    @ExceptionHandler(MessageNotFoundException::class)
-    @ResponseStatus(NOT_FOUND)
-    fun communityNotFound(e: MessageNotFoundException): Error = Error(NOT_FOUND.value(), e.message!!)
+    fun notFound(e: NoSuchElementException): Error = Error(NOT_FOUND.value(), e.message!!)
 }
