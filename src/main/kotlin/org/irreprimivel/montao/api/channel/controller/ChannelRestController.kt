465da1dcf00ee7df232eb469c18bcad1761a5706
truesik <truesik@gmail.com>
2017-05-28 15:22:23 +0300

    Методы с гетом теперь идут через JsonUtil который преобразует объекты в json с возможностью выбора нужных полей, Сделал page и limit необязательными параметрами, но по умолчанию теперь при вызове методом будет подставляться page = 1, а к limit = 30

--- src/main/kotlin/org/irreprimivel/montao/api/channel/controller/ChannelRestController.kt
+++ src/main/kotlin/org/irreprimivel/montao/api/channel/controller/ChannelRestController.kt
@@ -2,13 +2,12 @@ package org.irreprimivel.montao.api.channel.controller
 
 import org.irreprimivel.montao.api.channel.entity.Channel
 import org.irreprimivel.montao.api.channel.service.ChannelService
-import org.irreprimivel.montao.api.message.entity.Message
+import org.irreprimivel.montao.api.util.JsonUtil
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.util.UriComponentsBuilder
-import kotlin.streams.toList
 
 /**
  * Рестовый котроллерб который занимается выдачей данных по каналам.
@@ -17,18 +16,18 @@ import kotlin.streams.toList
  * - PUT    /channels/
  * - DELETE /channels/
  * - GET    /channels/
- * - GET    /channels/{title}
+ * - GET    /channels?community={communityTitle}
  * - GET    /channels/{id}/messages
- * - HEAD   /channels?title={title}
+ * - HEAD   /channels?title={title}&community={title}
  */
 @RestController
 @RequestMapping(value = "/channels")
 class ChannelRestController(val channelService: ChannelService) {
     @PostMapping(consumes = arrayOf(APPLICATION_JSON_UTF8_VALUE), produces = arrayOf(APPLICATION_JSON_UTF8_VALUE))
     fun add(@RequestBody channel: Channel, uriComponentsBuilder: UriComponentsBuilder): ResponseEntity<Channel> {
-        channelService.add(channel)
-        val location = uriComponentsBuilder.path("/channels/{title}").buildAndExpand(channel.title).toUri()
-        return ResponseEntity.created(location).build()
+        val createdChannel = channelService.add(channel)
+        val location = uriComponentsBuilder.path("/channels/{title}").buildAndExpand(createdChannel.title).toUri()
+        return ResponseEntity.created(location).body(createdChannel)
     }
 
     @PutMapping(consumes = arrayOf(APPLICATION_JSON_UTF8_VALUE), produces = arrayOf(APPLICATION_JSON_UTF8_VALUE))
@@ -41,34 +40,51 @@ class ChannelRestController(val channelService: ChannelService) {
     }
 
     @GetMapping(produces = arrayOf(APPLICATION_JSON_UTF8_VALUE))
-    fun findAll(@RequestParam page: Int, @RequestParam limit: Int): ResponseEntity<List<Channel>> {
+    fun findAll(@RequestParam(defaultValue = "1") page: Int,
+                @RequestParam(defaultValue = "30") limit: Int,
+                @RequestParam(required = false) fields: Array<out String>?): ResponseEntity<String> {
         val headers = HttpHeaders()
         with(headers) {
             set("X-Pagination-Count", channelService.totalCount().toString())
             set("X-Pagination-Page", page.toString())
             set("X-Pagination-Limit", limit.toString())
         }
-        return ResponseEntity.ok().headers(headers).body(channelService.findAll(page, limit))
+        val channels = channelService.findAll(page, limit)
+        return ResponseEntity.ok().headers(headers).body(JsonUtil.objectToJsonString(fields, "channel", channels))
     }
 
-    @GetMapping(value = "/{title}", produces = arrayOf(APPLICATION_JSON_UTF8_VALUE))
-    fun findByTitle(@PathVariable title: String): ResponseEntity<Channel> = ResponseEntity
-            .ok(channelService.findByTitle(title))
-
-    @GetMapping(value = "/{id}/messages", produces = arrayOf(APPLICATION_JSON_UTF8_VALUE))
-    fun findMessagesByChannel(@PathVariable id: Long,
-                              @RequestParam page: Int,
-                              @RequestParam limit: Int): ResponseEntity<List<Message>> {
+    /**
+     * Возвращает все каналы в сообществе.
+     */
+    @GetMapping(produces = arrayOf(APPLICATION_JSON_UTF8_VALUE))
+    fun findByCommunity(@RequestParam community: String,
+                        @RequestParam(defaultValue = "1") page: Int,
+                        @RequestParam(defaultValue = "30") limit: Int,
+                        @RequestParam(required = false) fields: Array<out String>?): ResponseEntity<String> {
         val headers = HttpHeaders()
         with(headers) {
-            set("X-Pagination-Count", channelService.findById(id).messages.stream().count().toString())
+            set("X-Pagination-Count", channelService.countByCommunity(community).toString())
             set("X-Pagination-Page", page.toString())
             set("X-Pagination-Limit", limit.toString())
         }
-        return ResponseEntity.ok().headers(headers).body(channelService.findById(id)
-                                                                 .messages.stream()
-                                                                 .skip(((page - 1) * limit).toLong())
-                                                                 .limit(limit.toLong())
-                                                                 .toList())
+        val channels = channelService.findByCommunity(community, page, limit)
+        return ResponseEntity.ok().headers(headers).body(JsonUtil.objectToJsonString(fields, "channel", channels))
     }
+
+    //    @GetMapping(value = "/{id}/messages", produces = arrayOf(APPLICATION_JSON_UTF8_VALUE))
+    //    fun findMessagesByChannel(@PathVariable id: Long,
+    //                              @RequestParam page: Int,
+    //                              @RequestParam limit: Int): ResponseEntity<List<Message>> {
+    //        val headers = HttpHeaders()
+    //        with(headers) {
+    //            set("X-Pagination-Count", channelService.findById(id).messages.stream().count().toString())
+    //            set("X-Pagination-Page", page.toString())
+    //            set("X-Pagination-Limit", limit.toString())
+    //        }
+    //        return ResponseEntity.ok().headers(headers).body(channelService.findById(id)
+    //                                                                 .messages.stream()
+    //                                                                 .skip(((page - 1) * limit).toLong())
+    //                                                                 .limit(limit.toLong())
+    //                                                                 .toList())
+    //    }
 }
