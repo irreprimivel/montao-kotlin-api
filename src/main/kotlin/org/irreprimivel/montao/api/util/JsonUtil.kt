1f423c1ed84c3bb5e2a16bf0d398bd946e6127bb
truesik <truesik@gmail.com>
2017-06-08 23:01:10 +0300

    ObjectMapper поменял на специальный для котлина

--- src/main/kotlin/org/irreprimivel/montao/api/util/JsonUtil.kt
+++ src/main/kotlin/org/irreprimivel/montao/api/util/JsonUtil.kt
@@ -1,14 +1,13 @@
 package org.irreprimivel.montao.api.util
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 object JsonUtil {
     fun objectToJsonString(expectedFields: Array<out String>?, jsonFilterName: String, pojo: Any): String {
         val except = if (expectedFields != null) SimpleBeanPropertyFilter.filterOutAllExcept(*expectedFields) else SimpleBeanPropertyFilter.serializeAll()
         val filter = SimpleFilterProvider().addFilter(jsonFilterName, except)
-        val mapper = ObjectMapper()
-        return mapper.writer(filter).writeValueAsString(pojo)
+        return jacksonObjectMapper().writer(filter).writeValueAsString(pojo)
     }
 }
